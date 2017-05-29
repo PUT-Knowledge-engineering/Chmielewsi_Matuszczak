8ecca8c9ed715e723eb8bcc49bd36815a65fbc8b
Lukasz Matuszczak <matuszczak.lukas@gmail.com>
2017-05-29 22:50:52 +0200

    Update manager

--- JsonManager.py
+++ JsonManager.py
@@ -4,29 +4,20 @@ Created on Mon May 29 20:33:28 2017
 
 @author: Lukasz
 """
-
 import json
 
-class DocumentationCreator:
+class JsonManager:
     def __init__(self, filePath):
         self.filePath = filePath
         self.globalJson = []
         
-    def addRow(self, dirPath, name, side, phase, histogram, size):
-        dict = {}
-        dict["dir"] = dirPath
-        dict["name"] = name
-        dict["side"] = side
-        dict["phase"] = phase
-        dict["histogram"] = histogram
-        dict["size"] = size
-        self.globalJson.append(dict)
+    def addRow(self, contextObject):
+        self.globalJson.append(contextObject)
+        
         
     def saveToFile(self):
-        with open(self.filePath, "a") as myfile:
+        with open(self.filePath, "w") as myfile:
             myfile.write(json.dumps(self.globalJson))
-        
-    
-        
-      
-        
+
+
+
